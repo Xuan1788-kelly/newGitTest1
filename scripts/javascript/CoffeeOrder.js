3375afc1e6b133369cd24041a418cfa8a2f65ffa
Zhu <Kelly.Zhu@arris.com>
2020-04-30 15:49:44 +0800

    Two cases are added for reference

--- scripts/javascript/CoffeeOrder.js
+++ scripts/javascript/CoffeeOrder.js
@@ -1,14 +1,40 @@
-$(function () {
+$(function (){
     var $orders = $('#orders');
+    var $name = $('#name');
+    var $drink = $('#drink');
 
     $.ajax({
         type: 'GET',
-        url: '/api/order',
+        url: 'http://127.0.0.1:5500/git_first_try/newGitTest1/api/orders.json',
         success: function(orders) {
             $.each(orders, function(i, order) {
-                $orders.append('<li>my order</li>');
-                //$orders.append('<li>name: ' + order.name + ', drink' + order.drink + '</li>');
+                var j = i + 1;
+                $orders.append('<li>order num ' +  j + '</li>');
+                $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>');
             });
+        },
+        error: function() {
+            alert("No such api");
         }
+    });
+
+    $('#add-orders').on('click', function() {
+        var order = {
+            name: $name.val(),
+            drink: $drink.val(),
+        };
+
+        $.ajax({
+            type: 'GET',
+            // 'POST' shall fail when the destination is a concrete JSON file
+            url: 'http://127.0.0.1:5500/git_first_try/newGitTest1/api/orders.json',
+            success: function() {
+                alert("start saving your order");
+                $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>');
+            },
+            error: function() {
+                alert("error saving order");
+            }
+        });
     });
 });
