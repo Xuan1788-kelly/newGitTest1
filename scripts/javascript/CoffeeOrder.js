d0901b0ba0ce1a3a84457b86265d5d67b742416e
Zhu <Kelly.Zhu@arris.com>
2020-05-14 11:05:41 +0800

    Create a simple coffee ordering web

---
+++ scripts/javascript/CoffeeOrder.js
@@ -0,0 +1,40 @@
+$(function (){
+    var $orders = $('#orders');
+    var $name = $('#username');
+    var $drink = $('#drink');
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://127.0.0.1:5500/git_first_try/newGitTest1/api/orders.json',
+        success: function(orders) {
+            $.each(orders, function(i, order) {
+                var j = i + 1;
+                $orders.append('<li>order num ' +  j + '</li>');
+                $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>');
+            });
+        },
+        error: function() {
+            alert("No such api");
+        }
+    });
+
+    $('#add-orders').on('click', function() {
+        var order = {
+            name: $name.val(),
+            drink: $drink.val(),
+        };
+
+        $.ajax({
+            type: 'GET',
+            // 'POST' shall fail when the destination is a concrete JSON file
+            url: 'http://127.0.0.1:5500/git_first_try/newGitTest1/api/orders.json',
+            success: function() {
+                alert("start saving your order");
+                $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>');
+            },
+            error: function() {
+                alert("error saving order");
+            }
+        });
+    });
+});
---
+++ scripts/javascript/coffee_canvas.js
@@ -0,0 +1,7 @@
+$(function() {
+    var tx=document.getElementById("firstCanvas");
+    var my_context=tx.getContext("2d");
+
+    my_context.font="bold 20px Arial";
+    my_context.fillText("Welcome to FishC", 33, 33);
+});
